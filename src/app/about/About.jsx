86c5312c189f530b7174e74451ccbf3b01f109b7
Ahmed Mohamed Saad Abdeldaiem <88617071+Ahmed-abdeldaiem@users.noreply.github.com>
2026-02-11 15:10:05 +0300

    first nextJS1-35

--- src/app/about/About.jsx
+++ src/app/about/About.jsx
@@ -183,7 +183,7 @@ export default function About() {
                     </button>
                     <Link
                       target={"_blank"}
-                      href="https://drive.google.com/file/d/16r0v2HODzY7ZLRVR-Ks4njjOFUzsMMta/view?usp=sharing"
+                      href="https://drive.google.com/file/d/1BBEoYERTflhKtBvB-y8LPT2DX7KvD2SE/view?usp=sharing"
                     >
                       <h3 className=" inline-block rounded-2xl px-2 md:px-5 cursor-pointer mx-1 bg-blue-100 bg-opacity-60 py-2 border-white border text-center z-20 text-blue-950 text-sm md:text-lg font-bold transition-all duration-500 hover:border-blue-950 hover:text-white hover:bg-opacity-50">
                         الملف التعريفي
@@ -610,7 +610,7 @@ export default function About() {
                     </button>
                     <Link
                       target={"_blank"}
-                      href="https://drive.google.com/file/d/16r0v2HODzY7ZLRVR-Ks4njjOFUzsMMta/view?usp=sharing"
+                      href="https://drive.google.com/file/d/1BBEoYERTflhKtBvB-y8LPT2DX7KvD2SE/view?usp=sharing"
                     >
                       <h3 className=" inline-block rounded-2xl px-2 md:px-5 cursor-pointer mx-1 bg-blue-100 bg-opacity-60 py-2 border-white border text-center z-20 text-blue-950 text-sm md:text-lg font-bold transition-all duration-500 hover:border-blue-950 hover:text-white hover:bg-opacity-50">
                       Company profile
--- src/app/components/PDFModal/PDFModal.jsx
+++ src/app/components/PDFModal/PDFModal.jsx
@@ -12,7 +12,7 @@ export default function PDFModal() {
   return (
     <>
       <div className="fixed bottom-20 left-4 z-50 cursor-pointer group">
-      <a  className="relative flex h-11 w-11" href="https://drive.google.com/file/d/16r0v2HODzY7ZLRVR-Ks4njjOFUzsMMta/view?usp=sharing" target={"_blank"}>
+      <a  className="relative flex h-11 w-11" href="https://drive.google.com/file/d/1BBEoYERTflhKtBvB-y8LPT2DX7KvD2SE/view?usp=sharing" target={"_blank"}>
       
           {/* Green ping circle behind */}
           <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-900 opacity-90"></span>
